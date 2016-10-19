afd9eccbd424ace0566d369b15bf045997c26127
Federico <federicoariasr@gmail.com>
2016-10-19 11:33:30 -0300

    initialll commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015' ]
+        }
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css!sass!'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+    ]
+  }
+};
