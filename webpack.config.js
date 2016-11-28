60ff43ee73f96151d447618d8a7a3f7546b6866f
Federico Arias <federicoariasr@gmail.com>
2016-11-28 20:31:27 -0300

    Blueprints del sitio (sin imgs)

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,12 @@ module.exports = {
       }, {
         test: /\.scss$/,
         loader: 'style!css!sass!'
-      },
-      {
+	  },
+	  {
+		  test: /\.(eot|svg|ttf|woff|woff2)$/,
+		  loader: 'file?name=/fonts/b[name].[ext]'
+	  },
+	  {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file?hash=sha512&digest=hex&name=[hash].[ext]',
