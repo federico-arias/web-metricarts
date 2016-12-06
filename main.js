c740cfe2f44f8f3dba4bed35a734df9661f74c64
Federico Arias <federico@debian>
2016-12-06 10:48:37 -0300

    Fuente Rasputin

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 import './styles/main.scss';
+
+require('./styles/fonts.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
 module.exports = {
-  entry: './main.js',
-  output: {
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015' ]
-        }
-      }, {
-        test: /\.scss$/,
-        loader: 'style!css!sass!'
-	  },
-	  {
-		  test: /\.(eot|svg|ttf|woff|woff2)$/,
-		  loader: 'file?name=/fonts/b[name].[ext]'
-	  },
-	  {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
-      }
-    ]
-  }
+	entry: './main.js',
+	output: {
+		filename: 'bundle.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				loader: 'style!css!sass!'
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				loader: 'file?name=/fonts2/[name].[ext]'
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file?hash=sha512&digest=hex&name=[hash].[ext]',
+					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+				]
+			},
+			{
+				test: /\.js?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015' ]
+				}
+			}
+		]
+	}
 };
