1d8ca99a661a43c47401b9dd30448688607f9960
Federico Arias <federicoariasr@gmail.com>
2017-01-04 17:59:12 -0300

    footer + header

---
+++ wordpress-theme/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<meta charset="utf-8" />
+<head>
+  <title>MetricArts - Inteligencia de negocios</title>
+	<link rel="stylesheet" href="style.css"></link>    
+</head>
+ 
+<body>
+	<div id="inicio" class="header">
+		<div class="header__menu page-navigation">
+			<div class="header__decorator">
+			</div>  
+			<div class="header__menu__logo">
+			</div> 
+			<nav class="header__menu__buttons">
+				<ul>
+					<li><a href="#inicio">Inicio</a></li>
+					<li><a href="#hacemos">Qué hacemos </a></li>
+					<li><a href="#quienes">Quiénes somos</a></li>
+					<li><a href="#blog">Blog</a></li>
+					<li><a href="#clientes">Clientes</a></li>
+					<li><a href="#contacto">Contacto</a></li>
+				</ul>
+
+			</nav>
+		</div>
+	</div>
