89757ca9fea056a4ac784311bc727ae7b98e4643
Federico Arias <federicoariasr@gmail.com>
2017-01-11 19:10:31 -0300

    Fix de SVG

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
 import './styles/main.scss';
 import './index.html';
 import './fonts/RaspoutineClassic.scss';
+import './cards.js';
 
-var fc = require('./demo.js');
-fc.foobar();
+/*var fc = require('./demo.js');
+fc.foobar();*/
+
+function isScrolledIntoView(el) {
+    var elemTop = el.getBoundingClientRect().top;
+    var elemBottom = el.getBoundingClientRect().bottom;
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+}
+
+function hideSvgPath(path) {
+	var length = path.getTotalLength();
+	path.style.transition = path.style.WebkitTransition = 'none';
+	path.style.strokeDasharray = length + ' ' + length;
+	path.style.strokeDashoffset = length;
+	path.getBoundingClientRect();
+	path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+}
+
+function extractPaths(svg) {
+	const doc = svg.getSVGDocument();
+    return doc.querySelectorAll("path"); 
+}
+
+
+const elems = document.querySelectorAll('object');
+Array.prototype.forEach.call(elems, (svg) => {	
+	svg.addEventListener('load', () =>  {
+		Array.prototype.forEach.call(extractPaths(svg), (path) => hideSvgPath(path))
+	})
+});
+
+window.onscroll = () => {
+	Array.prototype.forEach.call(elems, (svg) => {	
+		if (isScrolledIntoView(svg) && !svg.wasAnimated) {
+			Array.prototype.forEach.call(extractPaths(svg), p => p.style.strokeDashoffset = '0');
+			svg.wasAnimated = true;
+		}
+	});
+};
+
+['inicio', 'hacemos', 'quienes', 'blog', 'contacto'].forEach( x => {
+	if (!(document.querySelector == undefined)) {
+		document.getElementById(x + '-mnu').addEventListener('click', e => {
+			e.preventDefault();
+			document.getElementById(x).scrollIntoView({behavior: 'smooth'});
+		});
+	}
+});
